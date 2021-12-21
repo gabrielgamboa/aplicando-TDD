cfaff8e2d0a3b819c5695640f1557f812556beca
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-12-21 16:08:47 -0300

    adicionando middleware de tratativa de erros

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
 import "reflect-metadata";
@@ -6,11 +6,23 @@ import "./database";
 import "./shared/container";
 
 import { routes } from "./routes";
+import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message} `
+    });
+});
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
